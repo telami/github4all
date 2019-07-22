d60f1f32c1c5888aa33ed3eb03509f391dc698d5
zhuhongchao <zhuhongchao>
2019-07-22 13:27:55 +0800

    [Style] perfect style

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,22 +1,26 @@
 <template>
 	<view class="container">
 		<view class="trending-wrapper">
-			<view class="trending padding-xs margin-xs flex bg-white radius" v-for="(item,index) in projects" :key="index">
+			<view class="trending padding-xs margin-tb-sm flex bg-white radius" v-for="(item,index) in projects" :key="index">
 				<view class="detail-wrapper flex flex-direction justify-between flex-treble">
-					<view class="name text-bold padding-xs margin-xs">{{item.name}}</view>
-					<view class="desc padding-lr-xs margin-xs">{{item.description}}</view>
-					<view class="star-wrapper flex justify-between padding-xs margin-xs">
-						<text class="star cuIcon-favorfill">{{item.stars}}</text>
+					<view class="text-lg text-blue padding-xs margin-xs">
+						<text>{{item.author}}</text>/
+						<text class="text-bold">{{item.name}}</text>
+					</view>
+					<view class="cu-avatar-group padding-xs margin-xs">
+						<text class="padding-right">Built by</text>
+						<view class="cu-avatar round sm" v-for="(author,index) in item.builtBy" :key="index" :style="[{ backgroundImage:'url(' + author.avatar + ')' }]"></view>
+					</view>
+					<view class="desc padding-left-xs margin-xs">{{item.description}}</view>
+					<view class="star-wrapper text-sm flex justify-between padding-xs margin-xs">
+						<text v-if="item.language" class="cuIcon-round" :style="{color:item.languageColor}">{{item.language}}</text>
+						<text class="cuIcon-favorfill">{{item.stars}}</text>
 						<text class="star cuIcon-activityfill">{{item.forks}}</text>
 						<text class="star cuIcon-hotfill">{{item.currentPeriodStars}}</text>
 					</view>
 				</view>
-				<view class="image-wrapper flex-sub radius">
-					<image lazy-load="true" class="padding-xs margin-xs" :src="item.avatar"></image>
-					<view class="author margin-xs">{{item.author}}</view>
-					<view class="cu-avatar-group padding-xs margin-xs">
-						<view class="cu-avatar round sm" v-for="(author,index) in item.builtBy" :key="index" :style="[{ backgroundImage:'url(' + author.avatar + ')' }]"></view>
-					</view>
+				<view class="image-wrapper flex-sub flex flex-direction justify-center">
+					<image lazy-load="true" class="round margin-xs" :src="item.avatar"></image>
 				</view>
 			</view>
 		</view>
@@ -57,27 +61,6 @@
 	.container {
 		.trending-wrapper {
 			.trending {
-				height: 350rpx;
-
-				.image-wrapper {
-					width: 160rpx;
-					height: 160rpx;
-
-					image {
-						width: 160rpx;
-						height: 160rpx;
-					}
-
-					.author {
-						text-overflow: ellipsis;
-						overflow: hidden;
-						color: #000;
-						text-overflow: -o-ellipsis-lastline;
-						display: -webkit-box;
-						-webkit-line-clamp: 1;
-						-webkit-box-orient: vertical;
-					}
-				}
 
 				.detail-wrapper {
 					.desc {
@@ -86,8 +69,16 @@
 						color: #000;
 						text-overflow: -o-ellipsis-lastline;
 						display: -webkit-box;
-						-webkit-line-clamp: 2;
+						-webkit-line-clamp: 3;
 						-webkit-box-orient: vertical;
+					}
+				}
+
+				.image-wrapper {
+
+					image {
+						width: 160rpx;
+						height: 160rpx;
 					}
 				}
 			}
